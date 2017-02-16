94c5b301fbc64f2ab008ad63da04b3380ac5c18f
Shinnosuke Takagi <takagi@fringe81.com>
2017-02-16 14:56:13 +0900

    Initial commit.

---
+++ spec/markdown-hopper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MarkdownHopperView from '../lib/markdown-hopper-view';
+
+describe('MarkdownHopperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
